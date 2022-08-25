a9964add457b69c93de8d0c6e4defac9cb384fce
sgonzavar <sgonzalezvargas.7cc@gmail.com>
2022-08-25 16:26:29 -0500

    Feat: changes and solve logic problem

---
+++ ATM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleAppBank
+{
+    internal class ATM
+    {
+        public static string card = "", password = "", numc = "";
+        public static ArrayList arraytest = new ArrayList();
+        static void Main(string[] args)
+        {
+            People person1 = new People("pepito", "5353", "0000", 50000);
+            People person2 = new People("pepito", "5353", "0000", 50000);
+            People person3 = new People("pepito", "5353", "0000", 50000);
+            People person4 = new People("pepito", "5353", "0000", 50000);
+            People person5 = new People("pepito", "5353", "0000", 50000);
+
+            arraytest.Add(person1);
+            arraytest.Add(person2);
+            arraytest.Add(person3);
+            arraytest.Add(person4);
+            arraytest.Add(person5);
+
+            foreach (Object obj in arraytest)
+            {
+                Console.Write("{0}", obj);
+                Console.ReadKey();
+            }
+            Console.WriteLine("test to print {0}", arraytest.ToString());
+            Console.ReadKey();
+
+            for (int  index = 0;  index < arraytest.Count;  index++)
+            {
+                object o = arraytest[index];
+                Console.WriteLine("{0}", o.);
+            }
+                Console.ReadKey();
+        }
+    }
+}
---
+++ People.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleAppBank
+{
+    internal class People
+    {
+        private string _name;
+        private string _card;
+        private string _pssword;
+        private int _balance;
+
+        public People(string name, string card, string pssword, int balance)
+        {
+            _name = name;
+            _card = card;
+            _pssword = pssword;
+            _balance = balance;
+        }
+
+        public string Name { get => _name; set => _name = value; }
+        public string Card { get => _card; set => _card = value; }
+        public string Pssword { get => _pssword; set => _pssword = value; }
+        public int Balance { get => _balance; set => _balance = value; }
+    }
+}
